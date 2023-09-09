63b29202bcc87702d540c4ae3faf59ef84489978
WaterBongo <cutegato@riseup.net>
2023-09-08 22:09:05 -0700

    used working recorder

--- backend/main_server.py
+++ backend/main_server.py
@@ -1,6 +1,15 @@
 import flask,uuid
+from flask_cors import CORS
 from flask import request
 app = flask.Flask(__name__)
+CORS(app)
+
+
+
+
+
+def video_to_text(uuid):
+    pass
 
 
 
@@ -9,11 +18,20 @@ def index():
     return "sex"
 
 
+
+
+
+
+
+
+
 @app.route("/upload_video", methods=["POST"])
 def upload_video():
-    file = request.files['video']   
+    file = request.files['audio_data']   
     uuid_generated = str(uuid.uuid4())
-    file.save(f"./videos/{uuid_generated}.webm")
+    file.save(f"./videos/{uuid_generated}.wav")
     return {"id":uuid_generated}
+
+
 
 app.run("0.0.0.0",8080)
